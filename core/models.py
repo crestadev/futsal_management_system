138291c4949bf42da77c61d606ed071f6ae6f94d
Dev <nameofdeev@gmail.com>
2025-11-03 20:48:40 +0545

    Added models for field

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Field(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=150)
+    price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
