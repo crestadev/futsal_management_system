ebef1d4468e4af994307df70da94f3313347013e
Dev <nameofdeev@gmail.com>
2025-11-03 20:51:20 +0545

    Added urls in core

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('fields/', views.field_list, name='field_list'),
+    path('book/<int:field_id>/', views.book_field, name='book_field'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
 ]
