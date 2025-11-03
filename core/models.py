0f6452507466ec5ea9f8c6851f2924f945c36adb
Dev <nameofdeev@gmail.com>
2025-11-03 20:49:31 +0545

    Added booking for models

--- core/models.py
+++ core/models.py
@@ -9,3 +9,15 @@ class Field(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.field.name} ({self.date})"
