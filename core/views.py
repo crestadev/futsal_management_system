4c4998bd412bfc6a9d817a78994305f24c64222c
Dev <nameofdeev@gmail.com>
2025-11-03 20:53:20 +0545

    Added view of field_list

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Field
+
+
 
 def home(request):
     return render(request, 'home.html')
+
+def field_list(request):
+    fields = Field.objects.all()
+    return render(request, 'field_list.html', {'fields': fields})
