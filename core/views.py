3e862298373e9fe74a489cd74ab00720033e44f1
Dev <nameofdeev@gmail.com>
2025-11-04 21:15:58 +0545

    Added book_field

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Field
-
-
+from django.contrib.auth.decorators import login_required
+from .models import Field, Booking
+from datetime import datetime
 
 def home(request):
     return render(request, 'home.html')
@@ -9,3 +9,24 @@ def home(request):
 def field_list(request):
     fields = Field.objects.all()
     return render(request, 'field_list.html', {'fields': fields})
+
+@login_required
+def book_field(request, field_id):
+    field = get_object_or_404(Field, id=field_id)
+    
+    if request.method == 'POST':
+        date = request.POST.get('date')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+
+        Booking.objects.create(
+            user=request.user,
+            field=field,
+            date=date,
+            start_time=start_time,
+            end_time=end_time,
+            is_confirmed=True
+        )
+        return redirect('my_bookings')
+    
+    return render(request, 'book_field.html', {'field': field})
