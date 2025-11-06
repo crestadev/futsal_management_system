29aff46c7d6061c208437313c37eb8df723ae000
Dev <nameofdeev@gmail.com>
2025-11-06 21:00:40 +0545

    Setup url for register

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
     path('fields/', views.field_list, name='field_list'),
     path('book/<int:field_id>/', views.book_field, name='book_field'),
     path('my-bookings/', views.my_bookings, name='my_bookings'),
