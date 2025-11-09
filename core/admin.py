b1271a120c505b52b576373254fc5e110c0f4b39
Dev <nameofdeev@gmail.com>
2025-11-09 21:25:24 +0545

    Added booking in admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Field, Booking
 
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'field', 'date', 'start_time', 'end_time', 'status')
+    list_filter = ('status', 'date')
+    actions = ['approve_bookings', 'reject_bookings']
+
+    def approve_bookings(self, request, queryset):
+        queryset.update(status='approved')
+    approve_bookings.short_description = "Approve selected bookings"
+
+    def reject_bookings(self, request, queryset):
+        queryset.update(status='rejected')
+    reject_bookings.short_description = "Reject selected bookings"
+
 admin.site.register(Field)
-admin.site.register(Booking)
