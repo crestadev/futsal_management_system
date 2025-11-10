fcb5d306c287f76e3d63c7c68543b07b12d9de11
Dev <nameofdeev@gmail.com>
2025-11-10 23:21:05 +0545

    Added urls for update_booking

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('book/<int:field_id>/', views.book_field, name='book_field'),
     path('my-bookings/', views.my_bookings, name='my_bookings'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('update-booking/<int:booking_id>/<str:status>/', views.update_booking_status, name='update_booking_status'),
 
 ]
