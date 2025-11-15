ef9ec46227ab6cd38f51de443fad456a9b9eda16
Dev <nameofdeev@gmail.com>
2025-11-15 18:12:47 +0545

    Added forms for profileform

---
+++ core/forms.py
@@ -0,0 +1,13 @@
+# core/forms.py
+from django import forms
+from django.contrib.auth.models import User
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name':  forms.TextInput(attrs={'class': 'form-control'}),
+            'email':      forms.EmailInput(attrs={'class': 'form-control'}),
+        }
