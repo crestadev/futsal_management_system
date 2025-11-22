3b1027f21b27f4acd52d581594d44ca0f7f95050
Dev <nameofdeev@gmail.com>
2025-11-22 23:10:28 +0545

    Added generate qr view

--- core/views.py
+++ core/views.py
@@ -26,6 +26,10 @@ from openpyxl.utils import get_column_letter
 from django.core.mail import send_mail
 from django.conf import settings
 
+import qrcode
+import base64
+from io import BytesIO
+
 
 # ============================================================
 # PUBLIC PAGES
@@ -407,3 +411,18 @@ def send_booking_email(booking, event_type):
             [user_email],
             fail_silently=True,  # avoid crashing if email fails
         )
+def generate_qr_base64(data: str) -> str:
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=6,
+        border=2
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    qr_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return qr_base64
