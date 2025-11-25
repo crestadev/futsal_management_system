9791bfbec482efb32b66b7e1cdded67f7140210b
Dev <nameofdeev@gmail.com>
2025-11-25 14:43:04 +0545

    Added reviewform in forms

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,6 @@
-# core/forms.py
 from django import forms
 from django.contrib.auth.models import User
+from .models import Review
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -10,4 +10,12 @@ class ProfileForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name':  forms.TextInput(attrs={'class': 'form-control'}),
             'email':      forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'comment']
+        widgets = {
+            'rating': forms.NumberInput(attrs={'min':1, 'max':5, 'class':'form-control'}),
+            'comment': forms.Textarea(attrs={'class':'form-control', 'rows':3}),
         }
