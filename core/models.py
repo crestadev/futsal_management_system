de6ee74068be3861976006dae9458d692c6b5d4d
Dev <nameofdeev@gmail.com>
2025-11-26 22:34:41 +0545

    Added teamember in model

--- core/models.py
+++ core/models.py
@@ -4,6 +4,26 @@ from django.core.exceptions import ValidationError
 from datetime import datetime, timedelta
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class TeamMember(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('team', 'user')
+        
 class Review(models.Model):
     field = models.ForeignKey(Field, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
